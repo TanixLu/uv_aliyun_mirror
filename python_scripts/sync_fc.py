bd6bf21b10c39f0ee8f137428a2d2f64678f89af
Tanix <tanixlu@qq.com>
2025-05-08 10:44:29 +0800

    add sync_fc.py

--- python_scripts/sync_fc.py
+++ python_scripts/sync_fc.py
@@ -1,2 +1,9 @@
+from pathlib import Path
+
 from aliyun_utils import fc_upload
 
+fc_upload(
+    [Path("fc_server/target/x86_64-unknown-linux-musl/release/fc_server")],
+    "uv_mirror_service",
+    "uv_mirror_fc",
+)
